edcb948e429a5bbace75827350c05647c81f9ed2
JamisonCarvalho <jamisoncarvalho26@gmail.com>
2023-12-29 08:19:00 -0300

    adicionado o inicio da construcao da pokedex

---
+++ trabalhos/pokedex/assets/js/poke-api.js
@@ -0,0 +1,9 @@
+const pokeApi = {}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const URL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(URL)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .catch((error) => console.error(error))
+}
