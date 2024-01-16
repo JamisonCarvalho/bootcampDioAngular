487a85160679656e236acad83ea01d6a9518a024
JamisonCarvalho <jamisoncarvalho26@gmail.com>
2024-01-16 11:13:51 -0300

    criacao do blog

---
+++ trabalhos/blog/src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.scss'
+})
+export class SmallCardComponent implements OnInit{
+
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription: string = ""
+  @Input()
+  Id: string = "0"
+  
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+}
---
+++ trabalhos/blog/src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://media.moneytimes.com.br/uploads/2024/01/acoes-bolsa-b3-15.jpg",
+        "title": "Ibovespa (IBOV) abre em queda nesta segunda (15) sem Wall Street",
+        "description": "O Ibovespa (IBOV) opera em queda na abertura do pregão desta segunda-feira (15). Por volta das 10h12, o índice caía 0,39%, a 130.479 pontos."
+    },
+
+    {
+        "id": "2",
+        "photo": "https://media.seudinheiro.com/cdn-cgi/image/fit=contain,width=715&,format=auto/uploads/2018/12/leao-dormindo-1920X1080-628x353.jpg",
+        "title": "MUDANÇA DE REGRA",
+        "description": "Previdência privada: Agora você pode trocar a tabela de imposto de renda a qualquer momento; saiba quando vale a pena"
+    },
+
+    {
+        "id": "3",
+        "photo": "https://media.moneytimes.com.br/uploads/2024/01/acoes-77.jpg",
+        "title": "CARTEIRA RECOMENDADA",
+        "description": "5 ações para ter na carteira na semana e buscar retornos de até 93,4%, segundo Órama"
+    },
+
+    {
+        "id": "4",
+        "photo": "https://media.moneytimes.com.br/uploads/2024/01/doc.jpg",
+        "title": "TRANSAÇÕES VIA DOC VAI ACABAR",
+        "description": "Transações via DOC acabam nesta segunda (15); veja como migrar para o Pix"
+    }
+]
